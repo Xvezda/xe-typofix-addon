aafb7a2cdbb8fd530e7b61f6c91f4f0251880ca7
Xvezda <xvezda@naver.com>
2018-12-30 01:02:39 +0900

    Complete prototype

--- typofix.addon.php
+++ typofix.addon.php
@@ -3,26 +3,25 @@
 
 if (!defined('__XE__')) exit();
 
+
 if ($called_position == 'after_module_proc') {
+    if (Context::get('typo_fix') == 'off') return;
+
     if ($this->act == 'IS' && Context::get('is_keyword')
         || $this->act == 'dispBoardContent' && Context::get('search_keyword')) {
-        $parameter = '';
-        $keyword = '';
-
         switch ($this->module) {
         case 'board':
             $parameter = 'search_keyword';
-            $keyword = Context::get('search_keyword');
             break;
         case 'integration_search':
             $parameter = 'is_keyword';
-            $keyword = Context::get('is_keyword');
             break;
         default:
-            break;
+            return;
         }
-        if (!$keyword || !$parameter
-         || !$addon_info->client_id || !$addon_info->client_secret) return;
+        $keyword = Context::get($parameter);
+
+        if (!$addon_info->client_id || !$addon_info->client_secret) return;
 
         //echo $addon_info->client_id;
         $api_url = 'https://openapi.naver.com/v1/search/errata.xml';
@@ -53,6 +52,36 @@ if ($called_position == 'after_module_proc') {
 
         // Redirect to suggestion
         $this->setRedirectUrl(getNotEncodedUrl($parameter, $result, 'typo_keyword', $keyword));
+    } else if (Context::get('typo_keyword')) {
+        $this->setRedirectUrl(getNotEncodedUrl('typo_keyword', ''));
+    }
+}
+
+
+if ($called_position == 'before_display_content') {
+    if (Context::get('act') == 'IS') {
+        $parameter = 'is_keyword';
+    } else if (Context::get('search_target')) {
+        $parameter = 'search_keyword';
+    } else {
+        return;
+    }
+    $temp_output = $output;
+    $keyword = Context::get($parameter);
+
+    if (Context::get('typo_keyword') && $keyword) {
+        $typo_keyword = Context::get('typo_keyword');
+        Context::loadLang(_XE_PATH_ . 'addons/typofix/lang');
+
+        $prefix = '<!--#Meta:modules/';
+        // Find first position where module meta tags appear
+        $pos = strpos($temp_output, $prefix.$this->module);
+        if ($pos !== false) {
+            // Inject information box into top of content
+            $info_box = '<div id="typofix_info" style="line-height: 25px; padding: 15px 0; font-size: 14px">' . sprintf(Context::getLang('typofix_info_msg'), $keyword) . ' &nbsp;<a href="' . getAutoEncodedUrl($parameter, $typo_keyword, 'typo_keyword', '', 'typo_fix', 'off') . '">' . sprintf(Context::getLang('typofix_info_more_msg'), $typo_keyword) . '</a></div>';
+            $result = substr_replace($temp_output, $info_box, $pos, 0);
+            $output = $result;
+        }
     }
 }
 
