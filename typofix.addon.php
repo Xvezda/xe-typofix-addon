975d4bf9d0675c9c0bad7c1c22f6172f95c67b1c
Xvezda <xvezda@naver.com>
2018-12-30 16:14:29 +0900

    Add cache delete feature

--- typofix.addon.php
+++ typofix.addon.php
@@ -3,6 +3,53 @@
 
 if (!defined('__XE__')) exit();
 
+if ($called_position == 'after_module_proc') {
+    if ($this->module == 'addon' && $this->act == 'dispAddonAdminSetup'
+	    && Context::get('selected_addon') == 'typofix') {
+		$logged_info = Context::get('logged_info');
+        if ($logged_info->is_admin !== 'Y') return;
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'
+            && isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+            && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
+            $oCacheHandler = CacheHandler::getInstance();
+            if ($oCacheHandler->isSupport()) {
+                // FIXME: Find a way to clear cache just for addon
+                $oCacheHandler->truncate();
+            }
+            exit(0);
+        } else {
+            $customize_html = <<<EOD
+<script>
+xe.lang.cmd_delete = '%s';
+xe.lang.success_deleted = '%s';
+xe.lang.fail_to_delete = '%s';
+jQuery("#delete_cache").attr("type", "button").attr("value", xe.lang.cmd_delete)
+.addClass("x_btn").addClass("x_btn-danger")
+.on("click", function(e) {
+    jQuery.ajax({
+        url: current_url,
+        type: 'post',
+        dataType: 'json',
+        success: function(data) {
+            alert(xe.lang.success_deleted);
+        },
+        error: function(err) {
+            alert(xe.lang.fail_to_delete);
+        }
+    });
+    e.preventDefault();
+});
+</script>
+EOD;
+            Context::addHtmlFooter(
+                sprintf($customize_html, Context::getLang('cmd_delete'),
+                                         Context::getLang('success_deleted'),
+                                         Context::getLang('fail_to_delete')
+                )
+            );
+        }
+	}
+}
 
 if ($called_position == 'after_module_proc') {
     if (Context::get('typo_fix') == 'off') return;
@@ -24,33 +71,43 @@ if ($called_position == 'after_module_proc') {
 
         if (!$addon_info->client_id || !$addon_info->client_secret) return;
 
-        //echo $addon_info->client_id;
-        $api_url = 'https://openapi.naver.com/v1/search/errata.xml';
-        $api_url .= '?query=' . $keyword;
+        $result = '';
+        $oCacheHandler = CacheHandler::getInstance();
+        if ($oCacheHandler->isSupport()) {
+            $key = 'typofix:keyword:'. md5($keyword);
+            $result = $oCacheHandler->get($key);
+        }
+        if (!$result) {
+            $api_url = 'https://openapi.naver.com/v1/search/errata.xml';
+            $api_url .= '?query=' . $keyword;
 
-        $api_header = array();
-        $api_header['Host'] = 'openapi.naver.com';
-        $api_header['Pragma'] = 'no-cache';
-        $api_header['Accept'] = '*/*';
-        // Set naver open api requirements
-        $api_header['X-Naver-Client-Id'] = $addon_info->client_id;
-        $api_header['X-Naver-Client-Secret'] = $addon_info->client_secret;
+            $api_header = array();
+            $api_header['Host'] = 'openapi.naver.com';
+            $api_header['Pragma'] = 'no-cache';
+            $api_header['Accept'] = '*/*';
+            // Set naver open api requirements
+            $api_header['X-Naver-Client-Id'] = $addon_info->client_id;
+            $api_header['X-Naver-Client-Secret'] = $addon_info->client_secret;
 
-        $request_config = array();
-        $request_config['ssl_verify_peer'] = false;
+            $request_config = array();
+            $request_config['ssl_verify_peer'] = false;
 
-        $buff = FileHandler::getRemoteResource($api_url, null, 3, 'GET', null,
-                $api_header, array(), array(), $request_config);
+            $buff = FileHandler::getRemoteResource($api_url, null, 3, 'GET', null,
+                    $api_header, array(), array(), $request_config);
 
-        $xml = new XmlParser();
-        $xmlDoc = $xml->parse($buff);
+            $xml = new XmlParser();
+            $xmlDoc = $xml->parse($buff);
 
-        if (!$buff) return;
+            if (!$buff) return;
 
-        $result = $xmlDoc->result->item->errata->body;
+            $result = $xmlDoc->result->item->errata->body;
 
-        if (!$result) return;
-
+            if (!$result) return;
+            if ($oCacheHandler->isSupport()) {
+                $key = 'typofix:keyword:'. md5($keyword);
+                $oCacheHandler->put($key, $result);
+            }
+        }
         if ($addon_info->force_correction) {
             // Redirect to suggestion
             $this->setRedirectUrl(getNotEncodedUrl($parameter, $result, 'typo_keyword', $keyword));
